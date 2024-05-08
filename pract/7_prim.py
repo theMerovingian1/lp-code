5b91c01817ba1232ec9594d1f2a22c24138d9d78
theMerovingian1 <rishipb21@gmail.com>
2024-05-08 09:42:39 +0530

    Update 7_prim.py

--- pract/7_prim.py
+++ pract/7_prim.py
@@ -1,54 +1,58 @@
-import heapq
+import sys
 
 
 class Graph:
     def __init__(self, vertices):
         self.V = vertices
-        self.graph = [[] for _ in range(vertices)]
+        self.graph = [[0 for _ in range(vertices)] for _ in range(vertices)]
 
-    def add_edge(self, u, v, weight):
-        self.graph[u].append((v, weight))
-        self.graph[v].append((u, weight))
+    def printMST(self, parent):
+        print("Edge \tWeight")
+        for i in range(1, self.V):
+            print(parent[i], "-", i, "\t", self.graph[i][parent[i]])
 
-    def prim_mst(self):
-        visited = [False] * self.V
-        min_heap = []
-        mst_cost = 0
+    def minKey(self, key, mstSet):
+        min_val = sys.maxsize
+        min_index = -1
+        for v in range(self.V):
+            if key[v] < min_val and not mstSet[v]:
+                min_val = key[v]
+                min_index = v
+        return min_index
 
-        # Start with the vertex 0
-        heapq.heappush(min_heap, (0, 0))
+    def primMST(self):
+        key = [sys.maxsize] * self.V
+        parent = [None] * self.V
+        key[0] = 0
+        mstSet = [False] * self.V
+        parent[0] = -1
 
-        while min_heap:
-            weight, vertex = heapq.heappop(min_heap)
+        for _ in range(self.V):
+            u = self.minKey(key, mstSet)
+            mstSet[u] = True
+            for v in range(self.V):
+                if self.graph[u][v] and not mstSet[v] and key[v] > self.graph[u][v]:
+                    key[v] = self.graph[u][v]
+                    parent[v] = u
 
-            if not visited[vertex]:
-                mst_cost += weight
-                visited[vertex] = True
-
-                for neighbor, neighbor_weight in self.graph[vertex]:
-                    if not visited[neighbor]:
-                        heapq.heappush(min_heap, (neighbor_weight, neighbor))
-
-        return mst_cost
+        self.printMST(parent)
 
 
-def add_edges(graph):
-    while True:
-        u = int(input("Enter vertex u (-1 to finish): "))
-        if u == -1:
-            break
-        v = int(input("Enter vertex v: "))
-        weight = int(input("Enter weight: "))
-        graph.add_edge(u, v, weight)
+def get_input():
+    num_vertices = int(input("Enter the number of vertices: "))
+    g = Graph(num_vertices)
+    print("Enter the adjacency matrix (separate values by space):")
+    for i in range(num_vertices):
+        row = list(map(int, input().split()))
+        for j in range(num_vertices):
+            g.graph[i][j] = row[j]
+    return g
 
 
 def main():
-    num_vertices = int(input("Enter the number of vertices: "))
-    g = Graph(num_vertices)
-    add_edges(g)
-
-    mst_cost = g.prim_mst()
-    print("Minimum Spanning Tree Cost:", mst_cost)
+    print("Prim's Algorithm for Minimum Spanning Tree")
+    g = get_input()
+    g.primMST()
 
 
 if __name__ == "__main__":
