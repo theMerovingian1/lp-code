2510abd830ddb441336a1ee0ad11c4562be1a032
theMerovingian1 <rishipb21@gmail.com>
2024-05-08 09:49:03 +0530

    Update 9_dijkstra.py

--- pract/9_dijkstra.py
+++ pract/9_dijkstra.py
@@ -1,54 +1,52 @@
-import heapq
-
-
 class Graph:
     def __init__(self, vertices):
         self.V = vertices
-        self.graph = [[] for _ in range(vertices)]
+        self.graph = [[0 for _ in range(vertices)] for _ in range(vertices)]
 
-    def add_edge(self, u, v, weight):
-        self.graph[u].append((v, weight))
-        self.graph[v].append((u, weight))
+    def printSolution(self, dist):
+        print("Vertex \t Distance from Source")
+        for node in range(self.V):
+            print(node, "\t\t", dist[node])
 
-    def dijkstra_mst(self, src):
-        # Priority queue to store (distance, vertex) pairs
-        min_heap = [(0, src)]
-        mst_cost = 0
-        visited = set()
+    def minDistance(self, dist, sptSet):
+        min_dist = float('inf')
+        min_index = -1
+        for v in range(self.V):
+            if dist[v] < min_dist and not sptSet[v]:
+                min_dist = dist[v]
+                min_index = v
+        return min_index
 
-        while min_heap:
-            dist, vertex = heapq.heappop(min_heap)
+    def dijkstra(self, src):
+        dist = [float('inf')] * self.V
+        dist[src] = 0
+        sptSet = [False] * self.V
 
-            if vertex not in visited:
-                mst_cost += dist
-                visited.add(vertex)
+        for _ in range(self.V):
+            u = self.minDistance(dist, sptSet)
+            sptSet[u] = True
+            for v in range(self.V):
+                if (self.graph[u][v] > 0 and not sptSet[v] and
+                        dist[v] > dist[u] + self.graph[u][v]):
+                    dist[v] = dist[u] + self.graph[u][v]
 
-                for neighbor, weight in self.graph[vertex]:
-                    if neighbor not in visited:
-                        heapq.heappush(min_heap, (weight, neighbor))
-
-        return mst_cost
+        self.printSolution(dist)
 
 
-def add_edges(graph):
-    while True:
-        u = int(input("Enter vertex u (-1 to finish): "))
-        if u == -1:
-            break
-        v = int(input("Enter vertex v: "))
-        weight = int(input("Enter weight: "))
-        graph.add_edge(u, v, weight)
+def get_input():
+    num_vertices = int(input("Enter the number of vertices: "))
+    g = Graph(num_vertices)
+    print("Enter the adjacency matrix (separate values by space):")
+    for i in range(num_vertices):
+        g.graph[i] = list(map(int, input().split()))
+    return g
 
 
 def main():
-    num_vertices = int(input("Enter the number of vertices: "))
-    g = Graph(num_vertices)
-    add_edges(g)
-
-    src_vertex = int(input("Enter the source vertex: "))
-
-    mst_cost = g.dijkstra_mst(src_vertex)
-    print("Minimum Spanning Tree Cost:", mst_cost)
+    print("Dijkstra's Algorithm for Single Source Shortest Path")
+    g = get_input()
+    src = int(input("Enter the source vertex: "))
+    g.dijkstra(src)
 
 
 if __name__ == "__main__":
